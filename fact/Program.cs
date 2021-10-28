f8f928bbdbd2799a8b0ed91953feea5a39e525a8
Helga <Helga@DESKTOP-FN63M33>
2021-10-28 23:51:14 +0300

    Добавьте файлы проекта.

---
+++ fact/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace fact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = 1;
+            for (int i = 1; a >= i; i++)
+            {
+                b = b * i;
+            }
+            Console.WriteLine(b);
+        }
+    }
+}
