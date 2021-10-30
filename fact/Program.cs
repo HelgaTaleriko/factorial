a6d374671f12d452103e8b69788cef479f5b5898
Helga <Helga@DESKTOP-FN63M33>
2021-10-30 20:08:28 +0300

    Массив переделан

--- fact/Program.cs
+++ fact/Program.cs
@@ -13,11 +13,7 @@ namespace fact
                 int resultFact = Factorial(input);
                 int resultFibo = Fibo(input);
                 int resultFactRec = FactorialRecursive(input);
-                int[] result = new int[3];
-                result[0] = resultFact;
-                result[1] = resultFibo;
-                result[2] = resultFactRec;
-                //int[] result = new int { resultFact, resultFibo, resultFactRec }
+                int[] result = new int [] { resultFact, resultFibo, resultFactRec };
                 Console.WriteLine("Результат Фаакториала: "+ result[0]);
                 Console.WriteLine("Результат Фибоначи: " + result[1]);
                 Console.WriteLine("Результат рекурсивного Фаакториала: " + result[2]);
