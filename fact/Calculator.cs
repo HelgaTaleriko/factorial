79c313c91e875654a44fe9592fd4336005622dda
Helga <Helga@DESKTOP-FN63M33>
2021-10-30 20:50:22 +0300

    рефакторинг

--- fact/Calculator.cs
+++ fact/Calculator.cs
@@ -6,5 +6,47 @@ namespace fact
 {
     class Calculator
     {
+        public int Fibo(int a)
+        {
+            if (a <= 1)
+            { return 1; }
+            int fiboResult = Fibo(a - 1) + Fibo(a - 2);
+            return fiboResult;
+        }
+
+        public  int Factorial(int a)
+        {
+            int b = 1;
+            if (a < 0)
+            {
+                throw new Exception("введено отрицательное число");
+            }
+            else if (a % 1 != 0)
+            {
+                throw new Exception("введено дробное число");
+            }
+            for (int i = 1; i <= a; i++)
+            {
+                b *= i;
+            }
+            return b;
+        }
+        public int FactorialRecursive(int a)
+        {
+            if (a < 0)
+            {
+                throw new Exception("введено отрицательное число");
+            }
+            else if (a % 1 != 0)
+            {
+                throw new Exception("введено дробное число");
+            }
+            if (a <= 1)
+            { return 1; }
+            int result = a * FactorialRecursive(a - 1);
+            return result;
+        }
     }
+
 }
+
--- fact/Program.cs
+++ fact/Program.cs
@@ -11,54 +11,14 @@ namespace fact
                 Calculator calc1 = new Calculator();
                 Console.WriteLine("введите неотрицательное число");
                 int input = Convert.ToInt32(Console.ReadLine());
-                int resultFact = Factorial(input);
-                int resultFibo = Fibo(input);
-                int resultFactRec = FactorialRecursive(input);
-                int[] result = new int [] { resultFact, resultFibo, resultFactRec };
-                Console.WriteLine("Результат Фаакториала: "+ result[0]);
+                int resultFact = calc1.Factorial(input);
+                int resultFibo = calc1.Fibo(input);
+                int resultFactRec = calc1.FactorialRecursive(input);
+                int[] result = new int[] { resultFact, resultFibo, resultFactRec };
+                Console.WriteLine("Результат Фаакториала: " + result[0]);
                 Console.WriteLine("Результат Фибоначи: " + result[1]);
                 Console.WriteLine("Результат рекурсивного Фаакториала: " + result[2]);
             }
-        }
-        static int Fibo(int a)
-        {
-            if (a <= 1)
-            { return 1; }
-            int fiboResult = Fibo(a - 1) + Fibo(a - 2);
-            return fiboResult;
-        }
-
-        static int Factorial(int a)
-        {
-            int b = 1;
-            if (a < 0)
-            {
-                throw new Exception("введено отрицательное число");
-            }
-            else if (a % 1 != 0)
-            {
-                throw new Exception("введено дробное число");
-            }
-            for (int i = 1; i <= a; i++)
-            {
-                b *= i;
-            }
-            return b;
-        }
-        static int FactorialRecursive(int a)
-        {
-            if (a < 0)
-            {
-                throw new Exception("введено отрицательное число");
-            }
-            else if (a % 1 != 0)
-            {
-                throw new Exception("введено дробное число");
-            }
-            if (a <= 1)
-            { return 1; }
-            int result = a * FactorialRecursive(a - 1);
-            return result;
         }
     }
 }
